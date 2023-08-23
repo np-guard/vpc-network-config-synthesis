ff3607d2dfcf25ed9b8a43fab74cbfff57a16455
Elazar Gershuni <elazarg@ibm.com>
2023-08-23 14:08:09 +0300

    whitespace in tf files

--- test/data/single_conn1/nacl_single_expected.tf
+++ test/data/single_conn1/nacl_single_expected.tf
@@ -1,12 +1,12 @@
 resource "ibm_is_network_acl" "acl1" {
-  name = "acl1-${var.initials}"
+  name           = "acl1-${var.initials}"
   resource_group = var.resource_group_id
-  vpc = var.vpc_id
+  vpc            = var.vpc_id
   rules {
-    name = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,request,outbound,allow"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.10.0/24"
+    name        = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,request,outbound,allow"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.10.0/24"
     destination = "10.240.30.0/24"
     tcp {
       port_min = 443
@@ -14,10 +14,10 @@ resource "ibm_is_network_acl" "acl1" {
     }
   }
   rules {
-    name = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,response,inbound,allow"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.30.0/24"
+    name        = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,response,inbound,allow"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.30.0/24"
     destination = "10.240.10.0/24"
     tcp {
       source_port_min = 443
@@ -25,10 +25,10 @@ resource "ibm_is_network_acl" "acl1" {
     }
   }
   rules {
-    name = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,request,inbound,allow"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.10.0/24"
+    name        = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,request,inbound,allow"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.10.0/24"
     destination = "10.240.30.0/24"
     tcp {
       port_min = 443
@@ -36,10 +36,10 @@ resource "ibm_is_network_acl" "acl1" {
     }
   }
   rules {
-    name = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,response,outbound,allow"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.30.0/24"
+    name        = "c0,p0,[subnet1-ky->subnet3-ky],src0,dst0,response,outbound,allow"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.30.0/24"
     destination = "10.240.10.0/24"
     tcp {
       source_port_min = 443
