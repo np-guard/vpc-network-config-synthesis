8e24c3ac202e08602681d2d66dc6da762035cf6e
Elazar Gershuni <elazarg@ibm.com>
2023-10-04 10:59:27 +0300

    use only cidrs for externals

--- test/data/acl_testing5/nacl_single_expected.tf
+++ test/data/acl_testing5/nacl_single_expected.tf
@@ -544,14 +544,14 @@ resource "ibm_is_network_acl" "acl-1" {
     action      = "allow"
     direction   = "outbound"
     source      = "10.240.1.0/24"
-    destination = "8.8.8.8/0"
+    destination = "8.8.8.8/32"
   }
   # External. response to required-connections[1]: (segment need-dns)->(external dns); allowed-protocols[0]
   rules {
     name        = "rule55"
     action      = "allow"
     direction   = "inbound"
-    source      = "8.8.8.8/0"
+    source      = "8.8.8.8/32"
     destination = "10.240.1.0/24"
   }
   # External. required-connections[1]: (segment need-dns)->(external dns); allowed-protocols[0]
@@ -560,14 +560,14 @@ resource "ibm_is_network_acl" "acl-1" {
     action      = "allow"
     direction   = "outbound"
     source      = "10.240.64.0/24"
-    destination = "8.8.8.8/0"
+    destination = "8.8.8.8/32"
   }
   # External. response to required-connections[1]: (segment need-dns)->(external dns); allowed-protocols[0]
   rules {
     name        = "rule57"
     action      = "allow"
     direction   = "inbound"
-    source      = "8.8.8.8/0"
+    source      = "8.8.8.8/32"
     destination = "10.240.64.0/24"
   }
 }
