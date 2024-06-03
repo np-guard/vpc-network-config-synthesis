75dee88fb0803fbe616e9e9beac5f41b262934b7
Yair Slobodin <yair.slobodin@ibm.com>
2024-06-03 10:14:30 +0300

    updated cidrs

--- test/data/acl_cidr_segments1/nacl_single_expected.tf
+++ test/data/acl_cidr_segments1/nacl_single_expected.tf
@@ -39,7 +39,7 @@ resource "ibm_is_network_acl" "acl-testacl5-vpc--singleACL" {
     name        = "rule4"
     action      = "allow"
     direction   = "inbound"
-    source      = "10.240.3.0/23"
+    source      = "10.240.2.0/23"
     destination = "10.240.128.0/24"
   }
   # Internal. response to required-connections[0]: (segment cidrSegment)->(subnet testacl5-vpc/sub3-1); allowed-protocols[0]
@@ -48,7 +48,7 @@ resource "ibm_is_network_acl" "acl-testacl5-vpc--singleACL" {
     action      = "allow"
     direction   = "outbound"
     source      = "10.240.128.0/24"
-    destination = "10.240.3.0/23"
+    destination = "10.240.2.0/23"
   }
   # Internal. required-connections[1]: (subnet testacl5-vpc/sub2-1)->(segment cidrSegment); allowed-protocols[0]
   rules {
@@ -56,14 +56,14 @@ resource "ibm_is_network_acl" "acl-testacl5-vpc--singleACL" {
     action      = "allow"
     direction   = "outbound"
     source      = "10.240.64.0/24"
-    destination = "10.240.3.0/23"
+    destination = "10.240.2.0/23"
   }
   # Internal. response to required-connections[1]: (subnet testacl5-vpc/sub2-1)->(segment cidrSegment); allowed-protocols[0]
   rules {
     name        = "rule7"
     action      = "allow"
     direction   = "inbound"
-    source      = "10.240.3.0/23"
+    source      = "10.240.2.0/23"
     destination = "10.240.64.0/24"
   }
   # Internal. required-connections[1]: (subnet testacl5-vpc/sub2-1)->(segment cidrSegment); allowed-protocols[0]
