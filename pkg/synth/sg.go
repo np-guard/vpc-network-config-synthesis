f6bb71045a18c8a184073b9cc1ef4215d26ebffe
Yair Slobodin <yair.slobodin@ibm.com>
2024-07-01 17:22:45 +0300

    fixed

--- pkg/synth/sg.go
+++ pkg/synth/sg.go
@@ -18,14 +18,14 @@ const SGTypeNotSupported = "SG: src/dst of type %s is not supported."
 func MakeSG(s *ir.Spec, opt Options) *ir.SGCollection {
 	collections := []*ir.SGCollection{}
 	for c := range s.Connections {
-		collection := generateSGCollectionFromConnection(&s.Connections[c])
+		collection := generateSGCollectionFromConnection(&s.Defs, &s.Connections[c])
 		collections = append(collections, collection)
 	}
 	collections = append(collections, generateSGCollectionForBlockedResources(s))
 	return ir.MergeSGCollections(collections...)
 }
 
-func generateSGCollectionFromConnection(conn *ir.Connection) *ir.SGCollection {
+func generateSGCollectionFromConnection(s *ir.Definitions, conn *ir.Connection) *ir.SGCollection {
 	internalSrc := conn.Src.Type != ir.ResourceTypeExternal
 	internalDst := conn.Dst.Type != ir.ResourceTypeExternal
 	if !internalSrc && !internalDst {
@@ -40,8 +40,8 @@ func generateSGCollectionFromConnection(conn *ir.Connection) *ir.SGCollection {
 
 	result := ir.NewSGCollection()
 
-	srcEndpoints := updateEndpoints(conn.Src)
-	dstEndpoints := updateEndpoints(conn.Dst)
+	srcEndpoints := updateEndpoints(s, conn.Src)
+	dstEndpoints := updateEndpoints(s, conn.Dst)
 
 	for _, srcEndpoint := range srcEndpoints {
 		for _, dstEndpoint := range dstEndpoints {
@@ -57,14 +57,11 @@ func generateSGCollectionFromConnection(conn *ir.Connection) *ir.SGCollection {
 				}.String()
 
 				if internalSrc {
-					sgSrcName, ok := sgSelector(src).(ir.SGName)
-					if !ok {
-						log.Panicf("Source is not security group name: %v", src)
-					}
+					sgSrcName := ir.SGName(srcEndpoint.Name)
 					sgSrc := result.LookupOrCreate(sgSrcName)
 					sgSrc.Attached = []ir.ID{ir.ID(sgSrcName)}
 					rule := ir.SGRule{
-						Remote:      sgSelector(dst),
+						Remote:      remote(s, dstEndpoint),
 						Direction:   ir.Outbound,
 						Protocol:    trackedProtocol.Protocol,
 						Explanation: reason,
@@ -72,14 +69,11 @@ func generateSGCollectionFromConnection(conn *ir.Connection) *ir.SGCollection {
 					sgSrc.Rules = append(sgSrc.Rules, rule)
 				}
 				if internalDst {
-					sgDstName, ok := sgSelector(dst).(ir.SGName)
-					if !ok {
-						log.Panicf("Dst is not security group name: %v", dst)
-					}
+					sgDstName := ir.SGName(dstEndpoint.Name)
 					sgDst := result.LookupOrCreate(sgDstName)
 					sgDst.Attached = []ir.ID{ir.ID(sgDstName)}
 					rule := ir.SGRule{
-						Remote:      sgSelector(src),
+						Remote:      remote(s, srcEndpoint),
 						Direction:   ir.Inbound,
 						Protocol:    trackedProtocol.Protocol.InverseDirection(),
 						Explanation: reason,
@@ -102,17 +96,29 @@ func generateSGCollectionForBlockedResources(s *ir.Spec) *ir.SGCollection {
 	return result
 }
 
-func resourceRelevantToSG(e ir.ResourceType) bool {
-	return e == ir.ResourceTypeNIF || e == ir.ResourceTypeExternal || e == ir.ResourceTypeVPE
-}
-
-func updateEndpoints(resource ir.Resource) []ir.ConnResource {
+func updateEndpoints(s *ir.Definitions, resource ir.Resource) []ir.ConnResource {
 	if resource.Type == ir.ResourceTypeExternal {
 		return []ir.ConnResource{{Name: resource.Name, Addrs: resource.IPAddrs[0]}}
 	}
 	result := make([]ir.ConnResource, len(resource.IPAddrs))
 	for i, ip := range resource.IPAddrs {
-		result[i] = ir.ConnResource{Name: resource.Name, Addrs: ip}
+		name := resource.Name
+		// TODO: delete the if statement when there will be a support in VSIs with multiple NIFs
+		if nifDetails, ok := s.NIFs[resource.Name]; ok {
+			name = nifDetails.Instance
+		}
+		result[i] = ir.ConnResource{Name: name, Addrs: ip}
 	}
 	return result
+}
+
+func remote(s *ir.Definitions, resource ir.ConnResource) ir.RemoteType {
+	if _, ok := s.Externals[resource.Name]; ok {
+		return resource.Addrs
+	}
+	return ir.SGName(resource.Name)
+}
+
+func resourceRelevantToSG(e ir.ResourceType) bool {
+	return e == ir.ResourceTypeNIF || e == ir.ResourceTypeExternal || e == ir.ResourceTypeVPE
 }
