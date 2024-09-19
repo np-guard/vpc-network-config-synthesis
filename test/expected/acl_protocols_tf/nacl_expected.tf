a9172df4a00892c5d2d1be3f87d04efd2bd0e4f6
Yair Slobodin <yair.slobodin@ibm.com>
2024-09-19 15:12:37 +0300

    tf fmt

--- test/expected/acl_protocols_tf/nacl_expected.tf
+++ test/expected/acl_protocols_tf/nacl_expected.tf
@@ -1,84 +1,84 @@
 # test-vpc0/subnet0 [10.240.0.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc0--subnet0" {
-  name = "acl-test-vpc0--subnet0"
+  name           = "acl-test-vpc0--subnet0"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc0_id
+  vpc            = local.acl_synth_test-vpc0_id
   # Internal. required-connections[0]: (subnet test-vpc0/subnet0)->(subnet test-vpc0/subnet1); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.0.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.0.0/24"
     destination = "10.240.1.0/24"
     tcp {
     }
   }
   # Internal. response to required-connections[0]: (subnet test-vpc0/subnet0)->(subnet test-vpc0/subnet1); allowed-protocols[0]
   rules {
-    name = "rule1"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.1.0/24"
+    name        = "rule1"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.1.0/24"
     destination = "10.240.0.0/24"
     tcp {
     }
   }
   # Internal. required-connections[0]: (subnet test-vpc0/subnet0)->(subnet test-vpc0/subnet1); allowed-protocols[1]
   rules {
-    name = "rule2"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.0.0/24"
+    name        = "rule2"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.0.0/24"
     destination = "10.240.1.0/24"
   }
 }
 
 # test-vpc0/subnet1 [10.240.1.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc0--subnet1" {
-  name = "acl-test-vpc0--subnet1"
+  name           = "acl-test-vpc0--subnet1"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc0_id
+  vpc            = local.acl_synth_test-vpc0_id
   # Internal. required-connections[0]: (subnet test-vpc0/subnet0)->(subnet test-vpc0/subnet1); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.0.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.0.0/24"
     destination = "10.240.1.0/24"
     tcp {
     }
   }
   # Internal. response to required-connections[0]: (subnet test-vpc0/subnet0)->(subnet test-vpc0/subnet1); allowed-protocols[0]
   rules {
-    name = "rule1"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.1.0/24"
+    name        = "rule1"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.1.0/24"
     destination = "10.240.0.0/24"
     tcp {
     }
   }
   # Internal. required-connections[0]: (subnet test-vpc0/subnet0)->(subnet test-vpc0/subnet1); allowed-protocols[1]
   rules {
-    name = "rule2"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.0.0/24"
+    name        = "rule2"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.0.0/24"
     destination = "10.240.1.0/24"
   }
 }
 
 # test-vpc0/subnet2 [10.240.4.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc0--subnet2" {
-  name = "acl-test-vpc0--subnet2"
+  name           = "acl-test-vpc0--subnet2"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc0_id
+  vpc            = local.acl_synth_test-vpc0_id
   # Internal. required-connections[1]: (subnet test-vpc0/subnet2)->(subnet test-vpc0/subnet3); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.4.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.4.0/24"
     destination = "10.240.5.0/24"
     tcp {
       port_min = 8080
@@ -87,10 +87,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet2" {
   }
   # Internal. response to required-connections[1]: (subnet test-vpc0/subnet2)->(subnet test-vpc0/subnet3); allowed-protocols[0]
   rules {
-    name = "rule1"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.5.0/24"
+    name        = "rule1"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.5.0/24"
     destination = "10.240.4.0/24"
     tcp {
       source_port_min = 8080
@@ -99,10 +99,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet2" {
   }
   # Internal. required-connections[1]: (subnet test-vpc0/subnet2)->(subnet test-vpc0/subnet3); allowed-protocols[1]
   rules {
-    name = "rule2"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.4.0/24"
+    name        = "rule2"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.4.0/24"
     destination = "10.240.5.0/24"
     icmp {
       type = 3
@@ -113,15 +113,15 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet2" {
 
 # test-vpc0/subnet3 [10.240.5.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc0--subnet3" {
-  name = "acl-test-vpc0--subnet3"
+  name           = "acl-test-vpc0--subnet3"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc0_id
+  vpc            = local.acl_synth_test-vpc0_id
   # Internal. required-connections[1]: (subnet test-vpc0/subnet2)->(subnet test-vpc0/subnet3); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.4.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.4.0/24"
     destination = "10.240.5.0/24"
     tcp {
       port_min = 8080
@@ -130,10 +130,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet3" {
   }
   # Internal. response to required-connections[1]: (subnet test-vpc0/subnet2)->(subnet test-vpc0/subnet3); allowed-protocols[0]
   rules {
-    name = "rule1"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.5.0/24"
+    name        = "rule1"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.5.0/24"
     destination = "10.240.4.0/24"
     tcp {
       source_port_min = 8080
@@ -142,10 +142,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet3" {
   }
   # Internal. required-connections[1]: (subnet test-vpc0/subnet2)->(subnet test-vpc0/subnet3); allowed-protocols[1]
   rules {
-    name = "rule2"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.4.0/24"
+    name        = "rule2"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.4.0/24"
     destination = "10.240.5.0/24"
     icmp {
       type = 3
@@ -156,15 +156,15 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet3" {
 
 # test-vpc0/subnet4 [10.240.8.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc0--subnet4" {
-  name = "acl-test-vpc0--subnet4"
+  name           = "acl-test-vpc0--subnet4"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc0_id
+  vpc            = local.acl_synth_test-vpc0_id
   # Internal. required-connections[2]: (subnet test-vpc0/subnet4)->(subnet test-vpc0/subnet5); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.8.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.8.0/24"
     destination = "10.240.9.0/24"
     icmp {
       type = 15
@@ -173,10 +173,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet4" {
   }
   # Internal. response to required-connections[2]: (subnet test-vpc0/subnet4)->(subnet test-vpc0/subnet5); allowed-protocols[0]
   rules {
-    name = "rule1"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.9.0/24"
+    name        = "rule1"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.9.0/24"
     destination = "10.240.8.0/24"
     icmp {
       type = 16
@@ -185,10 +185,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet4" {
   }
   # Internal. required-connections[2]: (subnet test-vpc0/subnet4)->(subnet test-vpc0/subnet5); allowed-protocols[1]
   rules {
-    name = "rule2"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.8.0/24"
+    name        = "rule2"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.8.0/24"
     destination = "10.240.9.0/24"
     udp {
     }
@@ -197,15 +197,15 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet4" {
 
 # test-vpc0/subnet5 [10.240.9.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc0--subnet5" {
-  name = "acl-test-vpc0--subnet5"
+  name           = "acl-test-vpc0--subnet5"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc0_id
+  vpc            = local.acl_synth_test-vpc0_id
   # Internal. required-connections[2]: (subnet test-vpc0/subnet4)->(subnet test-vpc0/subnet5); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.8.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.8.0/24"
     destination = "10.240.9.0/24"
     icmp {
       type = 15
@@ -214,10 +214,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet5" {
   }
   # Internal. response to required-connections[2]: (subnet test-vpc0/subnet4)->(subnet test-vpc0/subnet5); allowed-protocols[0]
   rules {
-    name = "rule1"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.9.0/24"
+    name        = "rule1"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.9.0/24"
     destination = "10.240.8.0/24"
     icmp {
       type = 16
@@ -226,10 +226,10 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet5" {
   }
   # Internal. required-connections[2]: (subnet test-vpc0/subnet4)->(subnet test-vpc0/subnet5); allowed-protocols[1]
   rules {
-    name = "rule2"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.8.0/24"
+    name        = "rule2"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.8.0/24"
     destination = "10.240.9.0/24"
     udp {
     }
@@ -238,15 +238,15 @@ resource "ibm_is_network_acl" "acl-test-vpc0--subnet5" {
 
 # test-vpc1/subnet10 [10.240.64.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc1--subnet10" {
-  name = "acl-test-vpc1--subnet10"
+  name           = "acl-test-vpc1--subnet10"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc1_id
+  vpc            = local.acl_synth_test-vpc1_id
   # Internal. required-connections[3]: (subnet test-vpc1/subnet10)->(subnet test-vpc1/subnet11); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "outbound"
-    source = "10.240.64.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "outbound"
+    source      = "10.240.64.0/24"
     destination = "10.240.80.0/24"
     udp {
       port_min = 53
@@ -257,15 +257,15 @@ resource "ibm_is_network_acl" "acl-test-vpc1--subnet10" {
 
 # test-vpc1/subnet11 [10.240.80.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc1--subnet11" {
-  name = "acl-test-vpc1--subnet11"
+  name           = "acl-test-vpc1--subnet11"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc1_id
+  vpc            = local.acl_synth_test-vpc1_id
   # Internal. required-connections[3]: (subnet test-vpc1/subnet10)->(subnet test-vpc1/subnet11); allowed-protocols[0]
   rules {
-    name = "rule0"
-    action = "allow"
-    direction = "inbound"
-    source = "10.240.64.0/24"
+    name        = "rule0"
+    action      = "allow"
+    direction   = "inbound"
+    source      = "10.240.64.0/24"
     destination = "10.240.80.0/24"
     udp {
       port_min = 53
@@ -276,46 +276,46 @@ resource "ibm_is_network_acl" "acl-test-vpc1--subnet11" {
 
 # test-vpc2/subnet20 [10.240.128.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc2--subnet20" {
-  name = "acl-test-vpc2--subnet20"
+  name           = "acl-test-vpc2--subnet20"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc2_id
+  vpc            = local.acl_synth_test-vpc2_id
   # Deny all communication; subnet test-vpc2/subnet20[10.240.128.0/24] does not have required connections
   rules {
-    name = "rule0"
-    action = "deny"
-    direction = "inbound"
-    source = "0.0.0.0/0"
+    name        = "rule0"
+    action      = "deny"
+    direction   = "inbound"
+    source      = "0.0.0.0/0"
     destination = "10.240.128.0/24"
   }
   # Deny all communication; subnet test-vpc2/subnet20[10.240.128.0/24] does not have required connections
   rules {
-    name = "rule1"
-    action = "deny"
-    direction = "outbound"
-    source = "10.240.128.0/24"
+    name        = "rule1"
+    action      = "deny"
+    direction   = "outbound"
+    source      = "10.240.128.0/24"
     destination = "0.0.0.0/0"
   }
 }
 
 # test-vpc3/subnet30 [10.240.192.0/24]
 resource "ibm_is_network_acl" "acl-test-vpc3--subnet30" {
-  name = "acl-test-vpc3--subnet30"
+  name           = "acl-test-vpc3--subnet30"
   resource_group = local.acl_synth_resource_group_id
-  vpc = local.acl_synth_test-vpc3_id
+  vpc            = local.acl_synth_test-vpc3_id
   # Deny all communication; subnet test-vpc3/subnet30[10.240.192.0/24] does not have required connections
   rules {
-    name = "rule0"
-    action = "deny"
-    direction = "inbound"
-    source = "0.0.0.0/0"
+    name        = "rule0"
+    action      = "deny"
+    direction   = "inbound"
+    source      = "0.0.0.0/0"
     destination = "10.240.192.0/24"
   }
   # Deny all communication; subnet test-vpc3/subnet30[10.240.192.0/24] does not have required connections
   rules {
-    name = "rule1"
-    action = "deny"
-    direction = "outbound"
-    source = "10.240.192.0/24"
+    name        = "rule1"
+    action      = "deny"
+    direction   = "outbound"
+    source      = "10.240.192.0/24"
     destination = "0.0.0.0/0"
   }
 }
