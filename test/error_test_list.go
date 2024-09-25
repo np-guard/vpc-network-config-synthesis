695f1b5f6f227231d319e0165d96be92377d82f7
Yair Slobodin <yair.slobodin@ibm.com>
2024-09-25 12:32:20 +0300

    merging

--- test/error_test_list.go
+++ test/error_test_list.go
@@ -113,6 +113,19 @@ func errorTestsList() []testCase {
 			},
 		},
 
+		// external src and dst
+		{
+			testName:    "externals src and dst",
+			expectedErr: "both source and destination are external for connection",
+			args: &command{
+				cmd:        synth,
+				subcmd:     acl,
+				config:     "%s/externals/config_object.json",
+				spec:       "%s/externals/conn_spec.json",
+				outputFile: outputPath,
+			},
+		},
+
 		// unknown resource in spec
 		{
 			testName:    "unknown resource",
