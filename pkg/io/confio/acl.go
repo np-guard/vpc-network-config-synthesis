c9a434a5a0f6734972266a9c9bb227436b345d0d
Yair Slobodin <yair.slobodin@ibm.com>
2024-10-29 17:23:53 +0200

    json output

--- pkg/io/confio/acl.go
+++ pkg/io/confio/acl.go
@@ -18,8 +18,123 @@ import (
 	"github.com/np-guard/vpc-network-config-synthesis/pkg/utils"
 )
 
-func cidr(address *netset.IPBlock) *string {
-	return utils.Ptr(address.ToCidrListString())
+// WriteACL updates the given config_object file with synthesis/optimization results
+func (w *Writer) WriteACL(collection *ir.ACLCollection, _ string, isSynth bool) error {
+	var err error
+	if isSynth {
+		err = makeACLs(w.model, collection)
+	} else {
+		err = updateACLs(w.model, collection)
+	}
+	if err != nil {
+		return err
+	}
+	globalIndex = 0 // making test results more predictable
+	return w.writeModel()
+}
+
+// makeACLs calls updateSingleACL if we generated a single ACL,
+// o.w. it calls updateACL
+func makeACLs(model *configModel.ResourcesContainerModel, collection *ir.ACLCollection) error {
+	if len(model.SubnetList) == 0 {
+		return nil
+	}
+	subnet := model.SubnetList[0]
+	vpcName := *subnet.VPC.Name
+	aclName := ScopingString(vpcName, *subnet.Name)
+
+	// decide if we are in a single-ACL mode
+	if _, ok := collection.ACLs[vpcName][aclName]; ok {
+		return makeACL(model, collection)
+	}
+	return makeSingleACL(model, collection)
+}
+
+// updateACL writes all the generates nACLs to the config object
+func makeACL(model *configModel.ResourcesContainerModel, collection *ir.ACLCollection) error {
+	for _, subnet := range model.SubnetList {
+		vpcName := *subnet.VPC.Name
+		aclName := ScopingString(vpcName, *subnet.Name)
+
+		acl := collection.ACLs[vpcName][aclName]
+		aclItem, err := newACLItem(subnet, acl)
+		if err != nil {
+			return err
+		}
+		model.NetworkACLList = append(model.NetworkACLList, aclItem)
+		subnet.NetworkACL = newNACLRef(aclItem)
+	}
+
+	return nil
+}
+
+// updateACL writes  the generated nACL to the config object
+func makeSingleACL(model *configModel.ResourcesContainerModel, collection *ir.ACLCollection) error {
+	aclItem := &configModel.NetworkACL{}
+	var err error
+
+	for i, subnet := range model.SubnetList {
+		vpcName := *subnet.VPC.Name
+		acl := collection.ACLs[vpcName][ScopingString(vpcName, "singleACL")]
+
+		// if this is the first subnet being added to the ACL, add it to the list of network ACLs
+		// otherwise, add the subnet reference to the existing ACL item
+		if i == 0 {
+			aclItem, err = newACLItem(subnet, acl)
+			if err != nil {
+				return err
+			}
+			model.NetworkACLList = append(model.NetworkACLList, aclItem)
+		} else {
+			aclItem.Subnets = append(aclItem.Subnets, *subnetRef(subnet))
+		}
+		subnet.NetworkACL = newNACLRef(aclItem)
+	}
+	return nil
+}
+
+func newACLItem(subnet *configModel.Subnet, acl *ir.ACL) (*configModel.NetworkACL, error) {
+	ref := allocateRef()
+	rules, err := aclRules(acl)
+	if err != nil {
+		return nil, err
+	}
+	aclItem := configModel.NewNetworkACL(&vpcv1.NetworkACL{
+		CRN:           ref.CRN,
+		Href:          ref.Href,
+		ID:            ref.ID,
+		Name:          utils.Ptr(ir.ChangeScoping(acl.Name())),
+		ResourceGroup: subnet.ResourceGroup,
+		Rules:         rules,
+		Subnets:       []vpcv1.SubnetReference{*subnetRef(subnet)},
+		VPC:           subnet.VPC,
+	})
+	aclItem.Tags = []string{}
+	return aclItem, nil
+}
+
+func aclRules(acl *ir.ACL) ([]vpcv1.NetworkACLRuleItemIntf, error) {
+	rules := acl.Rules()
+	ruleItems := make([]vpcv1.NetworkACLRuleItemIntf, len(rules))
+
+	var next *vpcv1.NetworkACLRuleReference
+	for i := len(ruleItems) - 1; i >= 0; i-- {
+		name := utils.Ptr(fmt.Sprintf("rule%v", i))
+		ref := allocateRef()
+		current := &vpcv1.NetworkACLRuleReference{
+			Name: name,
+			Href: ref.Href,
+			ID:   ref.ID,
+		}
+		rule, err := makeACLRuleItem(rules[i], current, next)
+		if err != nil {
+			return nil, err
+		}
+		ruleItems[i] = rule
+		next = current
+	}
+
+	return ruleItems, nil
 }
 
 func makeACLRuleItem(rule *ir.ACLRule, current,
@@ -89,87 +204,6 @@ func makeACLRuleItem(rule *ir.ACLRule, current,
 	}
 }
 
-func aclRules(acl *ir.ACL) ([]vpcv1.NetworkACLRuleItemIntf, error) {
-	rules := acl.Rules()
-	ruleItems := make([]vpcv1.NetworkACLRuleItemIntf, len(rules))
-
-	var next *vpcv1.NetworkACLRuleReference
-	for i := len(ruleItems) - 1; i >= 0; i-- {
-		name := utils.Ptr(fmt.Sprintf("rule%v", i))
-		ref := allocateRef()
-		current := &vpcv1.NetworkACLRuleReference{
-			Name: name,
-			Href: ref.Href,
-			ID:   ref.ID,
-		}
-		rule, err := makeACLRuleItem(rules[i], current, next)
-		if err != nil {
-			return nil, err
-		}
-		ruleItems[i] = rule
-		next = current
-	}
-
-	return ruleItems, nil
-}
-
-func updateACLList(model *configModel.ResourcesContainerModel, collection *ir.ACLCollection) error {
-	if len(model.SubnetList) == 0 {
-		return nil
-	}
-	subnet := model.SubnetList[0]
-	vpcName := *subnet.VPC.Name
-	aclName := ScopingString(vpcName, *subnet.Name)
-
-	// decide if we are in a single-ACL mode
-	if _, ok := collection.ACLs[vpcName][aclName]; ok {
-		return updateACL(model, collection)
-	}
-	return updateSingleACL(model, collection)
-}
-
-func updateACL(model *configModel.ResourcesContainerModel, collection *ir.ACLCollection) error {
-	for _, subnet := range model.SubnetList {
-		vpcName := *subnet.VPC.Name
-		aclName := ScopingString(vpcName, *subnet.Name)
-
-		acl := collection.ACLs[vpcName][aclName]
-		aclItem, err := newACLItem(subnet, acl) // create a new ACL item for the subnet
-		if err != nil {
-			return err
-		}
-		model.NetworkACLList = append(model.NetworkACLList, aclItem)
-		subnet.NetworkACL = newNACLRef(aclItem)
-	}
-	globalIndex = 0 // making test results more predictable
-	return nil
-}
-
-func updateSingleACL(model *configModel.ResourcesContainerModel, collection *ir.ACLCollection) error {
-	aclItem := &configModel.NetworkACL{}
-	var err error
-
-	for i, subnet := range model.SubnetList {
-		vpcName := *subnet.VPC.Name
-		acl := collection.ACLs[vpcName][ScopingString(vpcName, "singleACL")]
-
-		// if this is the first subnet being added to the ACL, add it to the list of network ACLs
-		// otherwise, add the subnet reference to the existing ACL item
-		if i == 0 {
-			aclItem, err = newACLItem(subnet, acl)
-			if err != nil {
-				return err
-			}
-			model.NetworkACLList = append(model.NetworkACLList, aclItem)
-		} else {
-			aclItem.Subnets = append(aclItem.Subnets, *subnetRef(subnet))
-		}
-		subnet.NetworkACL = newNACLRef(aclItem)
-	}
-	globalIndex = 0 // making test results more predictable
-	return nil
-}
-
 func newNACLRef(aclItem *configModel.NetworkACL) *vpcv1.NetworkACLReference {
 	return &vpcv1.NetworkACLReference{
 		ID:   aclItem.ID,
@@ -177,26 +211,6 @@ func newNACLRef(aclItem *configModel.NetworkACL) *vpcv1.NetworkACLReference {
 		Href: aclItem.Href,
 		Name: utils.Ptr(*aclItem.Name),
 	}
-}
-
-func newACLItem(subnet *configModel.Subnet, acl *ir.ACL) (*configModel.NetworkACL, error) {
-	ref := allocateRef()
-	rules, err := aclRules(acl)
-	if err != nil {
-		return nil, err
-	}
-	aclItem := configModel.NewNetworkACL(&vpcv1.NetworkACL{
-		CRN:           ref.CRN,
-		Href:          ref.Href,
-		ID:            ref.ID,
-		Name:          utils.Ptr(ir.ChangeScoping(acl.Name())),
-		ResourceGroup: subnet.ResourceGroup,
-		Rules:         rules,
-		Subnets:       []vpcv1.SubnetReference{*subnetRef(subnet)},
-		VPC:           subnet.VPC,
-	})
-	aclItem.Tags = []string{}
-	return aclItem, nil
 }
 
 func subnetRef(subnet *configModel.Subnet) *vpcv1.SubnetReference {
@@ -209,9 +223,6 @@ func subnetRef(subnet *configModel.Subnet) *vpcv1.SubnetReference {
 	}
 }
 
-func (w *Writer) WriteACL(collection *ir.ACLCollection, _ string, _ bool) error {
-	if err := updateACLList(w.model, collection); err != nil {
-		return err
-	}
-	return w.writeModel()
+func cidr(address *netset.IPBlock) *string {
+	return utils.Ptr(address.ToCidrListString())
 }
