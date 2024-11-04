b2ab51cf1feebdf614ff27c123a1d5e5e286003f
Yair Slobodin <yair.slobodin@ibm.com>
2024-11-04 11:09:04 +0200

    wip

--- pkg/optimize/acl/acl.go
+++ pkg/optimize/acl/acl.go
@@ -6,8 +6,11 @@ SPDX-License-Identifier: Apache-2.0
 package acloptimizer
 
 import (
+	"fmt"
+
 	"github.com/np-guard/vpc-network-config-synthesis/pkg/ir"
 	"github.com/np-guard/vpc-network-config-synthesis/pkg/optimize"
+	"github.com/np-guard/vpc-network-config-synthesis/pkg/utils"
 )
 
 type (
@@ -27,5 +30,27 @@ func NewACLOptimizer(collection ir.Collection, aclName string) optimize.Optimize
 }
 
 func (a *aclOptimizer) Optimize() (ir.Collection, error) {
+	if a.aclName != "" {
+		for _, vpcName := range utils.SortedMapKeys(a.aclCollection.ACLs) {
+			if a.aclVPC != nil || a.aclVPC != &vpcName {
+				continue
+			}
+			if _, ok := a.aclCollection.ACLs[vpcName][a.aclName]; ok {
+				a.optimizeACL(vpcName, a.aclName)
+				return a.aclCollection, nil
+			}
+		}
+		return nil, fmt.Errorf("could no find %s acl", a.aclName)
+	}
+
+	for _, vpcName := range utils.SortedMapKeys(a.aclCollection.ACLs) {
+		for _, aclName := range utils.SortedMapKeys(a.aclCollection.ACLs[vpcName]) {
+			a.optimizeACL(vpcName, aclName)
+		}
+	}
 	return a.aclCollection, nil
+}
+
+func (a *aclOptimizer) optimizeACL(vpcName, aclName string) {
+
 }
