de8db1d19f0d7d14571ddbf961e0ab4634d4b3bd
Yair Slobodin <yair.slobodin@ibm.com>
2024-12-17 17:48:42 +0200

    wip

--- pkg/optimize/acl/cubesToRules.go
+++ pkg/optimize/acl/cubesToRules.go
@@ -6,6 +6,8 @@ SPDX-License-Identifier: Apache-2.0
 package acloptimizer
 
 import (
+	"slices"
+
 	"github.com/np-guard/models/pkg/ds"
 	"github.com/np-guard/models/pkg/netp"
 	"github.com/np-guard/models/pkg/netset"
@@ -13,8 +15,20 @@ import (
 	"github.com/np-guard/vpc-network-config-synthesis/pkg/optimize"
 )
 
-func aclCubesToRules(_ *aclCubesPerProtocol, _ ir.Direction) []*ir.ACLRule {
-	return nil
+func aclCubesToRules(cubes *aclCubesPerProtocol, direction ir.Direction) []*ir.ACLRule {
+	tcpRules := triplesToRulesTCPUDP(cubes.tcpAllow, direction, true)
+	udpRules := triplesToRulesTCPUDP(cubes.udpAllow, direction, false)
+	icmpRules := triplesToRulesICMP(cubes.icmpAllow, direction)
+	return slices.Concat(tcpRules, udpRules, icmpRules)
+}
+
+func triplesToRulesTCPUDP(tripleSet tcpudpTripleSet, direction ir.Direction, isTCP bool) []*ir.ACLRule {
+	partitions := minimalPartitionsTCPUDP(tripleSet, isTCP)
+	res := make([]*ir.ACLRule, len(partitions))
+	for i, t := range partitions {
+		res[i] = ir.NewACLRule(ir.Allow, direction, t.S1, t.S2, t.S3, "")
+	}
+	return res
 }
 
 func minimalPartitionsTCPUDP(t tcpudpTripleSet, isTCP bool) []ds.Triple[*netset.IPBlock, *netset.IPBlock, netp.TCPUDP] {
@@ -59,6 +73,15 @@ func breakTCPUDPTriple(t ds.Triple[*netset.IPBlock, *netset.IPBlock, *netset.Por
 	return res
 }
 
+func triplesToRulesICMP(tripleSet icmpTripleSet, direction ir.Direction) []*ir.ACLRule {
+	partitions := minimalPartitionsICMP(tripleSet)
+	res := make([]*ir.ACLRule, len(partitions))
+	for i, t := range partitions {
+		res[i] = ir.NewACLRule(ir.Allow, direction, t.S1, t.S2, t.S3, "")
+	}
+	return res
+}
+
 func minimalPartitionsICMP(t icmpTripleSet) []ds.Triple[*netset.IPBlock, *netset.IPBlock, netp.ICMP] {
 	leftPartitions := actualPartitionsICMP(ds.AsLeftTripleSet(t))
 	outerPartitions := actualPartitionsICMP(ds.AsOuterTripleSet(t))
@@ -93,7 +116,6 @@ func breakICMPTriple(t ds.Triple[*netset.IPBlock, *netset.IPBlock, *netset.ICMPS
 	for _, src := range t.S1.SplitToCidrs() {
 		for _, dst := range dstCidrs {
 			for _, icmp := range icmpPartitions {
-
 				a := ds.Triple[*netset.IPBlock, *netset.IPBlock, netp.ICMP]{S1: src, S2: dst, S3: icmp}
 				res = append(res, a)
 			}
