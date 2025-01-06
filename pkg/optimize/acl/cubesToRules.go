c324f4401c0eed56102a229f01a932c906bbabfb
Yair Slobodin <yair.slobodin@ibm.com>
2025-01-06 17:28:36 +0200

    docs

--- pkg/optimize/acl/cubesToRules.go
+++ pkg/optimize/acl/cubesToRules.go
@@ -16,7 +16,7 @@ import (
 )
 
 func aclCubesToRules(cubes *aclCubesPerProtocol, direction ir.Direction) []*ir.ACLRule {
-	// we we calculate the optimized deny cubes
+	// we calculate the optimized deny cubes
 	cubes.tcpDeny = ds.NewLeftTripleSet[*netset.IPBlock, *netset.IPBlock, *netset.TCPUDPSet]()
 	cubes.udpDeny = ds.NewLeftTripleSet[*netset.IPBlock, *netset.IPBlock, *netset.TCPUDPSet]()
 	cubes.icmpDeny = ds.NewLeftTripleSet[*netset.IPBlock, *netset.IPBlock, *netset.ICMPSet]()
